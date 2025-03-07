2642696582f9c9c73dca3a5c9b3ff1387a4734bd
Marlin12122009 <marlin.kalcher@sr.htlweiz.at>
2025-03-07 08:04:43 +0000

    Sort_numbers

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <inttypes.h>
 
-int main(int argc, char **argv) {
-  printf("This program was called with %d arguments.\n", argc);
-  printf("Hello World!\n");
+int main(int argc, char **argv)
+{
+
+  int32_t arr[5] = {1, 7, 3, 12, 13};
+  int32_t Ablage = 0;
+
+  for (int32_t count0 = 0; count0 < 5; count0 ++)
+  {
+    for (int32_t count = 0; count < 5; count++)
+    {
+      if (arr[count] > arr[count + 1])
+      {
+        Ablage = arr[count];
+        arr[count] = arr[count + 1];
+        arr[count + 1] = Ablage;
+      }
+    }
+  }
+  printf("%d,%d,%d,%d,%d\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
 }
